5f431ef7ad9615eb77f76ddda48049d7a4e6f95f
kensuke-nakamura-0107 <nakaken0107@yahoo.co.jp>
2021-04-10 12:34:44 +0900

    新規作成画面の表示
    新規作成画面の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_092509) do
+ActiveRecord::Schema.define(version: 2021_04_10_030554) do
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
